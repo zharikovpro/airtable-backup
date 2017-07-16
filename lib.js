2650fd30b824b81abf98f61285418c6410ecdd58
Andrey Zharikov <andrey@zharikov.pro>
2017-07-16 23:43:12 +0300

    Standard JS style

--- lib.js
+++ lib.js
@@ -8,43 +8,43 @@ const fs = require('fs')
 const path = require('path')
 const async = require('async')
 
-async function bases(email, password, api_info = true) {
-  const base_url = 'https://airtable.com'
+async function bases (email, password, apiInfo = true) {
+  const baseUrl = 'https://airtable.com'
 
   // cookies are required for the authentication to work
-  const request_with_cookies = axios.create({
+  const requestWithCookies = axios.create({
     jar: new tough.CookieJar(),
     withCredentials: true
-  });
+  })
 
-  const login_form = await request_with_cookies.get(`${base_url}/login`)
-  const _csrf = login_form.data.match(/name="_csrf"\s*value="(\S*)"/)[1]
+  const loginForm = await requestWithCookies.get(`${baseUrl}/login`)
+  const _csrf = loginForm.data.match(/name="_csrf"\s*value="(\S*)"/)[1]
 
-  const session = await request_with_cookies.post(`${base_url}/auth/login`, {_csrf, email, password})
+  const session = await requestWithCookies.post(`${baseUrl}/auth/login`, {_csrf, email, password})
   if (session.data.indexOf('redirectAfterSuccessfulLogin') === -1) {
-    throw 'NotLoggedIn'
+    throw new Error('NotLoggedIn')
   }
 
-  const app = await request_with_cookies.get(`${base_url}/auth/redirectAfterSuccessfulLogin`)
-  const init_data = JSON.parse(app.data.match(/initData.+?({.*})/)[1])
+  const app = await requestWithCookies.get(`${baseUrl}/auth/redirectAfterSuccessfulLogin`)
+  const initData = JSON.parse(app.data.match(/initData.+?({.*})/)[1])
 
-  let bases = _.mapValues(init_data['rawApplications'], (app) => {
+  let bases = _.mapValues(initData['rawApplications'], (app) => {
     return {
       name: app['name'],
-      tables: _.fromPairs(_.map(app['visibleTableOrder'], (table_id) => {
-        return [table_id, init_data.rawTables[table_id].name]
+      tables: _.fromPairs(_.map(app['visibleTableOrder'], (tableId) => {
+        return [tableId, initData.rawTables[tableId].name]
       }))
     }
   })
 
-  if (api_info) {
-    bases = await Promise.props(_.mapValues(bases, async (base, base_id) => {
-      const api_docs = await request_with_cookies.get(`${base_url}/${base_id}/api/docs#curl/introduction`)
-      const api_key = api_docs.data.match(/data-api-key="(\S*)"/)[1]
+  if (apiInfo) {
+    bases = await Promise.props(_.mapValues(bases, async (base, baseId) => {
+      const apiDocs = await requestWithCookies.get(`${baseUrl}/${baseId}/api/docs#curl/introduction`)
+      const apiKey = apiDocs.data.match(/data-api-key="(\S*)"/)[1]
 
       return _.merge(base, {
-        api_docs: api_docs.data,
-        api_key: api_key
+        apiDocs: apiDocs.data,
+        apiKey: apiKey
       })
     }))
   }
@@ -52,23 +52,23 @@ async function bases(email, password, api_info = true) {
   return bases
 }
 
-function table_records(api_key, base_id, table_id, done) {
-  let all_records = []
+function tableRecords (apiKey, baseId, tableId, done) {
+  let allRecords = []
 
-  const table = new Airtable({apiKey: api_key}).base(base_id)(table_id)
-  table.select().eachPage(function page(page_records, fetchNextPage) {
-    all_records = all_records.concat(_.map(page_records, (record) => { return record._rawJson }))
+  const table = new Airtable({apiKey: apiKey}).base(baseId)(tableId)
+  table.select().eachPage((pageRecords, fetchNextPage) => {
+    allRecords = allRecords.concat(_.map(pageRecords, (record) => { return record._rawJson }))
     setTimeout(fetchNextPage, 500) // API has a limit of 4rpm, wait a little more to be sure
   }, (error) => {
     if (error) {
       done(error, null)
     } else {
-      done(null, all_records)
+      done(null, allRecords)
     }
   })
 }
 
-function attachments_from_records(records) {
+function attachmentsFromRecords (records) {
   const attachments = _.map(records, (record) => {
     return _.map(record.fields, (field) => {
       if (_.isArray(field) && field[0] && field[0].filename && field[0].url) {
@@ -88,7 +88,7 @@ function attachments_from_records(records) {
   return _.compact(_.flattenDeep(attachments))
 }
 
-function create_dir(path) {
+function createDir (path) {
   if (fs.existsSync(path) === false) {
     fs.mkdirSync(path)
   }
@@ -96,56 +96,56 @@ function create_dir(path) {
   return path
 }
 
-function backup_attachments(attachments_path, attachments) {
+function backupAttachments (attachmentsPath, attachments) {
   async.eachLimit(attachments, 10, (attachment, done) => {
     axios.get(attachment.url, {responseType: 'arraybuffer'}).then((download) => {
-      const file_path = `${attachments_path}/${attachment.id}${path.extname(attachment.filename)}`
-      fs.writeFileSync(file_path, download.data)
+      const filePath = `${attachmentsPath}/${attachment.id}${path.extname(attachment.filename)}`
+      fs.writeFileSync(filePath, download.data)
       done()
     })
   })
 }
 
-function backup_table(backup_dir, base_id, base, table_id, attachments) {
-  table_records(base.api_key, base_id, table_id, (error, records) => {
-    const table_name = base.tables[table_id]
+function backupTable (backupDir, baseId, base, tableId, attachments) {
+  tableRecords(base.apiKey, baseId, tableId, (error, records) => {
+    const tableName = base.tables[tableId]
     if (error) {
-      console.log(`${base.name} ${table_name} ERROR: ${error} ×`)
+      console.log(`${base.name} ${tableName} ERROR: ${error} ×`)
     } else {
-      fs.writeFileSync(`${backup_dir}/${table_id}.json`, JSON.stringify(records, undefined, 2))
-      console.log(`${base.name} ${table_name} records ✔`)
+      fs.writeFileSync(`${backupDir}/${tableId}.json`, JSON.stringify(records, undefined, 2))
+      console.log(`${base.name} ${tableName} records ✔`)
 
       if (attachments) {
-        const dir = create_dir(`${backup_dir}/attachments`)
-        const attachments = attachments_from_records(records)
-        backup_attachments(dir, attachments)
+        const dir = createDir(`${backupDir}/attachments`)
+        const attachments = attachmentsFromRecords(records)
+        backupAttachments(dir, attachments)
       }
     }
   })
 }
 
-function backup_base(base_id, base, attachments = true) {
-  create_dir('backups')
-  create_dir(`backups/${base_id}`)
+function backupBase (baseId, base, attachments = true) {
+  createDir('backups')
+  createDir(`backups/${baseId}`)
 
-  const date_time = new Date()
-  const backup_dir = create_dir(`backups/${base_id}/${date_time.toISOString()}`)
+  const dateTime = new Date()
+  const backupDir = createDir(`backups/${baseId}/${dateTime.toISOString()}`)
 
-  fs.writeFileSync(`${backup_dir}/api_docs.html`, base.api_docs)
+  fs.writeFileSync(`${backupDir}/apiDocs.html`, base.apiDocs)
 
-  _.forEach(base.tables, (table_name, table_id) => {
-    backup_table(backup_dir, base_id, base, table_id, attachments)
+  _.forEach(base.tables, (tableName, tableId) => {
+    backupTable(backupDir, baseId, base, tableId, attachments)
   })
 }
 
-function backup_bases(email, password) {
+function backupBases (email, password) {
   bases(email, password).then((bases) => {
-    _.forEach(bases, (base, base_id) => {
-      backup_base(base_id, base, true)
+    _.forEach(bases, (base, baseId) => {
+      backupBase(baseId, base, true)
     })
   }).catch((error) => {
     console.log(error)
   })
 }
 
-module.exports = backup_bases
+module.exports = backupBases
